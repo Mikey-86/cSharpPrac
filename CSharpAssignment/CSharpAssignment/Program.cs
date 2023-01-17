37fdbf1fdc60af446b6e98eda1435721e4466e9d
Michael Francke <97046831+Mikey-86@users.noreply.github.com>
2023-01-17 15:43:16 +0200

    Add files via upload

---
+++ CSharpAssignment/CSharpAssignment/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using CSharpAssignment;
+
+//Variables
+int correctCounter = 0;
+int incorrectCounter = 0;
+string name = "";
+double percentage=0;
+
+
+//User enters name
+Console.WriteLine("Please enter your name");
+name = Console.ReadLine();
+
+List<Questions> source = new List<Questions>();  
+  
+using (StreamReader r = new StreamReader("/Users/michaelfrancke/RiderProjects/CSharpAssignment/CSharpAssignment/Questions.json"))  
+{  
+    string json = r.ReadToEnd();  
+    source = JsonSerializer.Deserialize<List<Questions>>(json);
+
+    //Console.WriteLine(source[0].question);
+    
+    foreach (Questions question in source)
+    {
+                
+        Console.WriteLine(question.question);
+        for (int i = 0; i < question.answers.Length; i++)
+        {
+            Console.WriteLine(i + " )" + question.answers[i]);
+        }
+        string uAnswer = Console.ReadLine();
+        if (int.Parse(uAnswer) == question.correctIndex - 1)
+        {
+            correctCounter++;
+            Console.WriteLine("Correct!");
+        }
+        else
+        {
+            incorrectCounter++;
+            Console.WriteLine("incorrect!");
+        }
+        
+         
+
+    }
+    Console.WriteLine(percentage);
+    percentage = correctCounter*(0.1);
+    Console.WriteLine(percentage);
+    
+    //Printing out answer
+    Console.WriteLine("--------------------------------------------------------");
+    Console.WriteLine("|    Name    | Total correct answers | Total incorrect answers |   Percentage");
+    Console.WriteLine("--------------------------------------------------------");
+    Console.WriteLine("|        "+ name + "    |       " + correctCounter +"               |       " + incorrectCounter + "                 |          " + percentage*100);
+    Console.WriteLine("--------------------------------------------------------");
+} 
---
+++ CSharpAssignment/CSharpAssignment/Questions.cs
@@ -0,0 +1,8 @@
+namespace CSharpAssignment;
+
+public class Questions
+{
+    public string question { get; set; }
+    public string[] answers { get; set; }
+    public int correctIndex { get; set; }
+}
